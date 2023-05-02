45c504fbe5a2a4812650d6b2b849bd858e9b31a4
eyabenaouicha <eyabenaouicha25@gmail.com>
2023-05-03 00:18:46 +0100

    update dashboard

---
+++ Utilisateur/index.php
@@ -0,0 +1,142 @@
+<?php
+include_once("../connexion.php");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>THE 7 </title>
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">The 7</span>
+    </div>
+      <ul class="nav-links">
+        <li>
+          <a href="../dashboard.php" >
+            <i class='bx bx-grid-alt' ></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="active">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Utilisateur</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-list-ul' ></i>
+            <span class="links_name">Reservation</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">Chambre</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-coin-stack' ></i>
+            <span class="links_name">Categorie</span>
+          </a>
+        </li>
+        <li class="log_out">
+          <a href="#">
+            <i class='bx bx-log-out'></i>
+            <span class="links_name">Log out</span>
+          </a>
+        </li>
+      </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <div class="search-box">
+        <input type="text" placeholder="Search...">
+        <i class='bx bx-search' ></i>
+      </div>
+      <div class="profile-details">
+        <img src="images/profile.jpg" alt="">
+        <span class="admin_name">Prem Shahi</span>
+        <i class='bx bx-chevron-down' ></i>
+      </div>
+    </nav>
+
+    <div class="home-content" style="width: 147%!important;">
+    <?php
+$pdo = new connect();
+$query = "select * from utilisateur";
+$pdostmt = $pdo->prepare($query);
+$pdostmt->execute();
+?>
+
+      <div class="sales-boxes" >
+        <div class="recent-sales box">
+          <div class="title">Utilisateur</div>
+         <a href="create.php"><img src="../fonts/user-plus-solid.svg" alt="" style="width:30px;heigth:30px"></a>
+          <div class="sales-details">
+            
+           <table>
+            <tr>
+                <th>Id</th>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Email</th>
+                <th>CIN</th>
+                <th>Numero</th>
+                <th>type</th>
+                <th>Operations</th>
+            </tr>
+            <?php
+        $ligne = $pdostmt->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($ligne);
+        foreach ($ligne as $value) {
+        ?>
+            <tr>
+                <td> <?php echo $value["id"]; ?></td>
+                <td><?php echo $value["nom"]; ?></td>
+                <td><?php echo $value["prenom"]; ?></td>
+                <td><?php echo $value["email"]; ?></td>
+                <td><?php echo $value["cin"]; ?></td>
+                <td><?php echo $value["numero"]; ?></td>
+                <td><?php echo $value["type"]; ?></td>
+                <td><a href="update.php?id=<?php echo $value["id"] ?>"><img src="../fonts/pen-to-square-solid.svg"  style="width:25px;heigth:25px"alt=""></a> <a href="delete.php?id=<?php echo $value["id"] ?>"><img src="../fonts/trash-solid.svg"  style="width:20px;heigth:20px"alt=""></a></td>
+                <td></td>
+            </tr>
+        <?php } ?>
+           </table>
+          </div>
+          <div class="button">
+            <a href="#">See All</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <script>
+   let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+}else
+  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+ </script>
+
+</body>
+</html>
---
+++ dashboard.php
@@ -0,0 +1,292 @@
+<?php
+include_once("connexion.php");
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>THE 7 </title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">The 7</span>
+    </div>
+      <ul class="nav-links">
+        <li>
+          <a href="#" class="active">
+            <i class='bx bx-grid-alt' ></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="Utilisateur/index.php">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Utilisateur</span>
+          </a>
+        </li>
+        <li>
+        <a href="#">
+            <i class='bx bx-list-ul' ></i>
+            <span class="links_name">Reservation</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">Chambre</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-coin-stack' ></i>
+            <span class="links_name">Categorie</span>
+          </a>
+        </li>
+        <li class="log_out">
+          <a href="#">
+            <i class='bx bx-log-out'></i>
+            <span class="links_name">Log out</span>
+          </a>
+        </li>
+        
+      </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <!-- <div class="search-box">
+        <input type="text" placeholder="Search...">
+        <i class='bx bx-search' ></i>
+      </div> -->
+      <div class="profile-details">
+        <img src="images/profile.jpg" alt="">
+        <span class="admin_name">Prem Shahi</span>
+        <i class="fa-solid fa-user"></i>
+      </div>
+    </nav>
+
+    <div class="home-content">
+    <?php
+$pdo = new connect();
+// statistique utilisateurs 
+$queryUser = "select count(*) as countUser from utilisateur";
+$pdostmtUser = $pdo->prepare($queryUser);
+$pdostmtUser->execute();
+$countUser = $pdostmtUser->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($countUser[0]["countUser"]);
+
+
+// statistique reservation
+$queryReservation = "select count(*) as countReservation from reservation";
+$pdostmtReservation = $pdo->prepare($queryReservation);
+$pdostmtReservation->execute();
+$countReservation = $pdostmtReservation->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($countReservation[0]["countReservation"]);
+
+
+// statistique chambre
+$queryChambre = "select count(*) as countChambre from chambre";
+$pdostmtChambre = $pdo->prepare($queryChambre);
+$pdostmtChambre->execute();
+$countChambre = $pdostmtChambre->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($countChambre[0]["countChambre"]);
+
+
+// statistique categorie
+$queryCategorie = "select count(*) as countCategorie from categorie";
+$pdostmtCategorie = $pdo->prepare($queryCategorie);
+$pdostmtCategorie->execute();
+$countCategorie = $pdostmtCategorie->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($countCategorie[0]["countCategorie"]);
+
+
+?>
+      <div class="overview-boxes">
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Utilisateur</div>
+            <div class="number"><?php echo $countUser[0]["countUser"]; ?></div>
+            <!-- <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div> -->
+          </div>
+          <i class='bx bx-cart-alt cart'></i>
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Reservation</div>
+            <div class="number"><?php echo $countReservation[0]["countReservation"]; ?></div>
+            <!-- <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div> -->
+          </div>
+          <i class='bx bxs-cart-add cart two' ></i>
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Chambre</div>
+            <div class="number"><?php echo $countChambre[0]["countChambre"]; ?></div>
+            <!-- <div class="indicator">
+              <i class='bx bx-up-arrow-alt'></i>
+              <span class="text">Up from yesterday</span>
+            </div> -->
+          </div>
+          <i class='bx bx-cart cart three' ></i>
+        </div>
+        <div class="box">
+          <div class="right-side">
+            <div class="box-topic">Categorie</div>
+            <div class="number"><?php echo $countCategorie[0]["countCategorie"]; ?></div>
+            <!-- <div class="indicator">
+              <i class='bx bx-down-arrow-alt down'></i>
+              <span class="text">Down From Today</span>
+            </div> -->
+          </div>
+          <i class='bx bxs-cart-download cart four' ></i>
+        </div>
+      </div>
+
+      <div class="sales-boxes">
+        <div class="recent-sales box">
+          <div class="title">Recent Sales</div>
+          <div class="sales-details">
+            <ul class="details">
+              <li class="topic">Date</li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+              <li><a href="#">02 Jan 2021</a></li>
+            </ul>
+            <ul class="details">
+            <li class="topic">Customer</li>
+            <li><a href="#">Alex Doe</a></li>
+            <li><a href="#">David Mart</a></li>
+            <li><a href="#">Roe Parter</a></li>
+            <li><a href="#">Diana Penty</a></li>
+            <li><a href="#">Martin Paw</a></li>
+            <li><a href="#">Doe Alex</a></li>
+            <li><a href="#">Aiana Lexa</a></li>
+            <li><a href="#">Rexel Mags</a></li>
+             <li><a href="#">Tiana Loths</a></li>
+          </ul>
+          <ul class="details">
+            <li class="topic">Sales</li>
+            <li><a href="#">Delivered</a></li>
+            <li><a href="#">Pending</a></li>
+            <li><a href="#">Returned</a></li>
+            <li><a href="#">Delivered</a></li>
+            <li><a href="#">Pending</a></li>
+            <li><a href="#">Returned</a></li>
+            <li><a href="#">Delivered</a></li>
+             <li><a href="#">Pending</a></li>
+            <li><a href="#">Delivered</a></li>
+          </ul>
+          <ul class="details">
+            <li class="topic">Total</li>
+            <li><a href="#">$204.98</a></li>
+            <li><a href="#">$24.55</a></li>
+            <li><a href="#">$25.88</a></li>
+            <li><a href="#">$170.66</a></li>
+            <li><a href="#">$56.56</a></li>
+            <li><a href="#">$44.95</a></li>
+            <li><a href="#">$67.33</a></li>
+             <li><a href="#">$23.53</a></li>
+             <li><a href="#">$46.52</a></li>
+          </ul>
+          </div>
+          <div class="button">
+            <a href="#">See All</a>
+          </div>
+        </div>
+        <div class="top-sales box">
+          <div class="title">Top Seling Product</div>
+          <ul class="top-sales-details">
+            <li>
+            <a href="#">
+              <img src="images/sunglasses.jpg" alt="">
+              <span class="product">Vuitton Sunglasses</span>
+            </a>
+            <span class="price">$1107</span>
+          </li>
+          <li>
+            <a href="#">
+               <img src="images/jeans.jpg" alt="">
+              <span class="product">Hourglass Jeans </span>
+            </a>
+            <span class="price">$1567</span>
+          </li>
+          <li>
+            <a href="#">
+             <img src="images/nike.jpg" alt="">
+              <span class="product">Nike Sport Shoe</span>
+            </a>
+            <span class="price">$1234</span>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/scarves.jpg" alt="">
+              <span class="product">Hermes Silk Scarves.</span>
+            </a>
+            <span class="price">$2312</span>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/blueBag.jpg" alt="">
+              <span class="product">Succi Ladies Bag</span>
+            </a>
+            <span class="price">$1456</span>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/bag.jpg" alt="">
+              <span class="product">Gucci Womens's Bags</span>
+            </a>
+            <span class="price">$2345</span>
+          <li>
+            <a href="#">
+              <img src="images/addidas.jpg" alt="">
+              <span class="product">Addidas Running Shoe</span>
+            </a>
+            <span class="price">$2345</span>
+          </li>
+<li>
+            <a href="#">
+             <img src="images/shirt.jpg" alt="">
+              <span class="product">Bilack Wear's Shirt</span>
+            </a>
+            <span class="price">$1245</span>
+          </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script>
+   let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("bx-menu" ,"bx-menu-alt-right");
+}else
+  sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+ </script>
+
+</body>
+</html>
