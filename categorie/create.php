c7eaf132a37cf828403495189a7392a6cc6defdd
saintfranky <wadhahsalhi999@gmail.com>
2023-05-04 20:01:41 +0100

    htjt

--- categorie/create.php
+++ categorie/create.php
@@ -1,29 +1,169 @@
+
 <?php 
-include_once("connexion.php");
-if(!empty($_POST["inputnom"])&&!empty($_POST["inputprenom"])&&!empty($_POST["inputemail"])){
+include_once("../connexion.php");
+if(!empty($_POST["inputtype"])){
     $pdo=new connect();
-    $query="insert into categorie(type) values(:nom,:prenom,:email)";
+    $query="insert into categorie(type) values(:type)";
     $pdostmt=$pdo->prepare($query);
-    $pdostmt->execute(["nom"=>$_POST["inputnom"],"prenom"=>$_POST["inputprenom"],"email"=>$_POST["inputemail"]]);
-    // $pdostmt->bindValue(":nom",$_POST["inputnom"],PDO::PARAM_STR);
-    // $pdostmt->bindValue(":prenom",$_POST["inputprenom"],PDO::PARAM_STR);
-    // $pdostmt->bindValue(":email",$_POST["inputemail"],PDO::PARAM_STR);
-    // $pdostmt->execute();
-    $pdostmt->closeCursor(); //liberer pdostmt
-    header("location:index.php"); //redirection vers test.php
+    $pdostmt->execute(["type"=>$_POST["inputtype"]]);
+    $pdostmt->closeCursor(); 
+    header("location:index.php"); 
 }
 ?>
-<html>
-    <head>
 
-    </head>
-    <body>
-        <form action="createuser.php" method="post">
-            <input type="text" name="inputnom" placeholder="nom">
-            <input type="text" name="inputprenom" placeholder="prenom">
-            <input type="text" name="inputemail" placeholder="email">
-            <input type="submit" name="submit" value="Ajouter">
-        </form>
-      
-    </body>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>THE 7 </title>
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo-details">
+            <i class='bx bxl-c-plus-plus'></i>
+            <span class="logo_name">The 7</span>
+        </div>
+        <ul class="nav-links">
+            <li>
+                <a href="../dashboard.php">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="links_name">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href=../utilisateur/index.php >
+                    <i class='bx bx-box'></i>
+                    <span class="links_name">Utilisateur</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-list-ul'></i>
+                    <span class="links_name">Reservation</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-pie-chart-alt-2'></i>
+                    <span class="links_name">Chambre</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="active">
+                    <i class='bx bx-coin-stack'></i>
+                    <span class="links_name">Categorie</span>
+                </a>
+            </li>
+            <li class="log_out">
+                <a href="#">
+                    <i class='bx bx-log-out'></i>
+                    <span class="links_name">Log out</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <section class="home-section">
+        <nav>
+            <div class="sidebar-button">
+                <i class='bx bx-menu sidebarBtn'></i>
+                <span class="dashboard">Dashboard</span>
+            </div>
+            <div class="search-box">
+                <input type="text" placeholder="Search...">
+                <i class='bx bx-search'></i>
+            </div>
+            <div class="profile-details">
+                <img src="images/profile.jpg" alt="">
+                <span class="admin_name">Prem Shahi</span>
+                <i class='bx bx-chevron-down'></i>
+            </div>
+        </nav>
+
+        <div class="home-content" style="width: 147%!important;">
+            <?php
+            $pdo = new connect();
+            $query = "select * from categorie";
+            $pdostmt = $pdo->prepare($query);
+            $pdostmt->execute();
+            ?>
+
+            <div class="sales-boxes">
+                <div class="recent-sales box">
+                    <div class="title">Ajouter categorie</div>
+                    <br>
+                    <div class="sales-details">
+
+                        <div class="wrapper">
+                            <div class="registration_form">
+                                        <form>
+                                            <div class="form_wrap">
+                                                <div class="input_grp">
+                                                        <div class="input_wrap">
+                                                            <label for="fname">First Name</label>
+                                                            <input type="text" id="fname">
+                                                        </div>
+                                                           
+                                                </div>
+                                                    
+                                                       
+                                                           
+                                            </div><br>
+                                  
+                    <div class="button">
+                        <a href="#"><input type="submit" value="Register Now" class="submit_btn" style="background: #0d6bd700;border:none;color:white">
+</a>
+                    </div>
+                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <script>
+        let sidebar = document.querySelector(".sidebar");
+        let sidebarBtn = document.querySelector(".sidebarBtn");
+        sidebarBtn.onclick = function() {
+            sidebar.classList.toggle("active");
+            if (sidebar.classList.contains("active")) {
+                sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+            } else
+                sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+
+        function myFunction() {
+            // Declare variables
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
+
+</body>
+
 </html>
