604a2478e117cb7bd7bdee51ec54bbdff53ebd12
eyabenaouicha <eyabenaouicha25@gmail.com>
2023-05-05 14:15:37 +0100

    vv

--- hotelvf.sql
+++ hotelvf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 04 mai 2023 à 16:46
+-- Généré le :  ven. 05 mai 2023 à 08:58
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -33,7 +33,17 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `type`) VALUES
+(1, 'Individuelle'),
+(2, 'Double'),
+(3, 'A3'),
+(4, 'A4');
 
 -- --------------------------------------------------------
 
@@ -45,12 +55,19 @@ DROP TABLE IF EXISTS `chambre`;
 CREATE TABLE IF NOT EXISTS `chambre` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `num` int(11) NOT NULL,
-  `type` enum('individuelle','a2','a3','') NOT NULL,
   `prix` float NOT NULL,
   `idCategorie` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idCategorie` (`idCategorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `chambre`
+--
+
+INSERT INTO `chambre` (`id`, `num`, `prix`, `idCategorie`) VALUES
+(1, 150, 300, 3),
+(2, 545, 225, 2);
 
 -- --------------------------------------------------------
 
@@ -63,7 +80,15 @@ CREATE TABLE IF NOT EXISTS `facture` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `prix` float NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `facture`
+--
+
+INSERT INTO `facture` (`id`, `prix`) VALUES
+(1, 500),
+(2, 300);
 
 -- --------------------------------------------------------
 
@@ -85,7 +110,16 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   KEY `idUtilisateur` (`idUtilisateur`),
   KEY `idChambre` (`idChambre`),
   KEY `idFacture` (`idFacture`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `dateDebut`, `dateFin`, `formule`, `idUtilisateur`, `idChambre`, `etat`, `idFacture`) VALUES
+(2, '2023-01-08', '2023-01-19', 'pensionComplete', 2, 1, 'confirmer', 1),
+(4, '2023-01-01', '2023-01-19', 'demiPension', 1, 2, 'confirmer', 1),
+(5, '2023-01-01', '2023-01-19', 'pensionComplete', 3, 1, 'confirmer', 2);
 
 -- --------------------------------------------------------
 
@@ -102,16 +136,19 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `numero` int(11) NOT NULL,
   `cin` int(11) NOT NULL,
   `type` enum('admin','client','equipe','') NOT NULL,
+  `mdp` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `numero`, `cin`, `type`) VALUES
-(1, 'Ben Aouicha', 'Eya', 'eya@gmail.com', 111122, 11111, 'admin'),
-(2, 'Ben Aouicha', 'Eya', 'eya@gmail.com', 111122, 11111, 'admin');
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `numero`, `cin`, `type`, `mdp`) VALUES
+(1, 'Ben Aouicha', 'Eya', 'eya@gmail.com', 25252525, 11427049, 'admin', '123456'),
+(2, 'Jaouadi', 'Sinda', 'sinda@gmail.com', 111122, 11457896, 'client', '123456'),
+(3, 'Salhi', 'Wadhah', 'wadhah25@gmail.com', 22245689, 1235649, 'equipe', '5645798'),
+(4, 'Bellil', 'Maryem', 'maryem@gmail.com', 185976775, 9874556, 'client', 'fieiohfeio');
 
 --
 -- Contraintes pour les tables déchargées
